a9366bd3766e07289777cc06bf6e0f083e5d8011
Thanh Le <thanhlevan0203@gmail.com>
2022-08-06 10:58:28 -0700

    Created Instument Spec class

---
+++ InstrumentSpec.java
@@ -0,0 +1,9 @@
+public abstract class InstrumentSpec {
+    private Builder builder;
+    private String model;
+    private Type type;
+    private Wood backWood;
+    private Wood topWood;
+
+    
+}
